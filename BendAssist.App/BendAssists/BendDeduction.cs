ed275294e4a54897edc9daf42c527e49f36e57dc
Raanesh <raaneshkumar.k.r@metamation.com>
2024-06-26 14:30:29 +0530

    Implemented Bend Deduction

--- BendAssist.App/BendAssists/BendDeduction.cs
+++ BendAssist.App/BendAssists/BendDeduction.cs
@@ -1,4 +1,6 @@
 ﻿using BendAssist.App.Model;
+using BendAssist.App.Utils;
+using static BendAssist.App.Model.EOrientation;
 
 namespace BendAssist.App.BendAssists;
 
@@ -11,20 +13,157 @@ public sealed class BendDeduction : BendAssist {
    #region Methods --------------------------------------------------
    public override bool Assisted () {
       if (mPart is null) return false;
-      if (mAlgorithm is EBDAlgorithm.PartiallyDistributed) {
-         var newBLines = new List<BendLine> ();
-         foreach (var bl in mPart.BendLines) {
-         }
-      } else { }
+      Execute ();
       return true;
+   }
+
+   /// <summary>Applies bend deduction for the bend lines and the edges</summary>
+   public override void Execute () {
+      var totalBD = 0.0;
+      int verBLCount = 0, horBLCount = 0;
+      List<BendLine> newBendLines = []; List<PLine> newPLines = [];
+      if (mAlgorithm is EBDAlgorithm.PartiallyDistributed) { // Handles only horizontal bend lines
+         var tmp = mPart?.BendLines.Select (bl => (BendLine)bl.Clone ()).Reverse ().ToList ();
+         newBendLines.AddRange (GetTranslatedBLines (tmp!, out totalBD, out horBLCount, out verBLCount));
+         var centroidY = mPart!.Centroid.Y;
+         foreach (var pLine in mPart.PLines) {
+            var newPLine = pLine.StartPoint.Y < centroidY && pLine.EndPoint.Y < centroidY ? pLine.Translated (0, totalBD)
+                                                                                          : pLine;
+            if (pLine.Orientation is Vertical) {
+               if (newPLine.StartPoint.Y < newPLine.EndPoint.Y) newPLine = newPLine.Trimmed (0, totalBD, 0, 0);
+               else newPLine = newPLine.Trimmed (0, 0, 0, totalBD);
+            }
+            newPLines.Add ((PLine)newPLine);
+         }
+      } else { // Equally distributed algorithm - Handles horizontal and vertical bend lines
+         var bendLines = mPart?.BendLines;
+         var blCount = bendLines!.Count;
+         // The area between the two innermost bend lines is considered the base.
+         var bottomBLines = bendLines.Take (blCount / 2).Reverse ().ToList (); // Bend lines on the bottom and left side of the base
+         var topBLines = bendLines.TakeLast (blCount - bottomBLines.Count).ToList (); // Bend lines on the top and right side of the base
+         var tempPLines = new List<PLine> ();
+         // Applies bend deduction from the top and right side of the part
+         ApplyEqDistributedBD (ref newBendLines, topBLines, ref tempPLines, ref newPLines, EBLLocation.Top);
+         if (bottomBLines.Count == 0) {  // Incase of a single bend line part
+            foreach (var pLine in tempPLines) newPLines.Add (pLine);
+            if (mPart != null) newPLines.Add (mPart.PLines.Except (tempPLines).First ());
+         }
+         // Applies bend deduction from the bottom and left side of the part
+         ApplyEqDistributedBD (ref newBendLines, bottomBLines, ref tempPLines, ref newPLines, EBLLocation.Bottom);
+      }
+      mProcessedPart = new ProcessedPart (newPLines, newBendLines, 0, EBendAssist.BendDeduction);
+   }
+
+   /// <summary>Applies bend deduction for the bend lines and the edges equally on both sides of the bend lines</summary>
+   // newBLines - Collection of bend deducted bend lines, bLines - Bend lines which are to be bend deducted.
+   // tempPLines - Temporary collection of perpendicular pLines which have to be trimmed from the other side.
+   // newPLines - Collection of bend deducted pLines, blLoc - Bend Line location.
+   void ApplyEqDistributedBD (ref List<BendLine> newBLines, List<BendLine> bLines, ref List<PLine> tempPLines, ref List<PLine> newPLines, EBLLocation blLoc) {
+      if (bLines.Count == 0) return;
+      if (mPart is null) return;
+      // Bend Deduction on bend lines
+      newBLines.AddRange (GetTranslatedBLines (bLines, out double totalBD, out int horBLCount, out int verBLCount, isNegOff: blLoc is EBLLocation.Top));
+      // Bend Deduction on edges
+      foreach (var pLine in GetProfilePLines (mPart, blLoc, verBLCount > 0, horBLCount > 0)) {
+         var pLOrient = pLine.Orientation;
+         var (dx, dy) = blLoc is EBLLocation.Top ? pLOrient is Horizontal ? (0.0, -totalBD) : (-totalBD, 0)
+                                                 : pLOrient is Horizontal ? (0.0, totalBD) : (totalBD, 0);
+         newPLines.Add ((PLine)pLine.Translated (dx, dy)); // Parallel pLines
+         switch (blLoc) {
+            case EBLLocation.Top:
+               foreach (var idx in GetCPIndices (pLine, mPart.PLines)) { // Trims the connected perpendicular edges
+                  var conPLine = mPart.PLines.Where (cC => cC.Index == idx).First ();
+                  PLine trimmed;
+                  if (pLOrient is Horizontal)
+                     trimmed = conPLine.StartPoint.Y < pLine.StartPoint.Y ? (PLine)conPLine.Trimmed (0, 0, 0, -totalBD)
+                                                                          : (PLine)conPLine.Trimmed (0, -totalBD, 0, 0);
+                  else
+                     trimmed = conPLine.StartPoint.X < pLine.StartPoint.X ? (PLine)conPLine.Trimmed (0, 0, -totalBD, 0)
+                                                                          : (PLine)conPLine.Trimmed (-totalBD, 0, 0, 0);
+                  tempPLines.Add (trimmed);
+               }
+               break;
+            case EBLLocation.Bottom:
+               foreach (var idx in GetCPIndices (pLine, mPart.PLines)) {
+                  PLine conPLine;
+                  if (tempPLines.Any (tC => tC.Index == idx)) // checks whether the edge is trimmed from the other side or not
+                     conPLine = tempPLines.Where (tC => tC.Index == idx).First ();
+                  else {
+                     conPLine = mPart.PLines.Where (pC => pC.Index == idx).First (); // in case of new edges to be trimmed
+                     foreach (var tC in tempPLines) newPLines.Add (tC);
+                  }
+                  PLine trimmed;
+                  if (pLOrient is Horizontal)
+                     trimmed = conPLine.StartPoint.Y > pLine.StartPoint.Y ? (PLine)conPLine.Trimmed (0, 0, 0, totalBD)
+                                                                          : (PLine)conPLine.Trimmed (0, totalBD, 0, 0);
+                  else
+                     trimmed = conPLine.StartPoint.X > pLine.StartPoint.X ? (PLine)conPLine.Trimmed (0, 0, totalBD, 0)
+                                                                          : (PLine)conPLine.Trimmed (totalBD, 0, 0, 0);
+                  newPLines.Add (trimmed);
+               }
+               break;
+         }
+      }
+   }
+
+   // To be moved to Utils.cs
+   /// <summary>Returns the array of indices of the connected pLines from the given pLine</summary>
+   static int[] GetCPIndices (PLine refPLine, List<PLine> pLines) {
+      var (start, end) = (refPLine.StartPoint, refPLine.EndPoint);
+      return pLines.Where (c => c.Index != refPLine.Index && (c.HasVertex (start) || c.HasVertex (end))).Select (c => c.Index).ToArray ();
    }
    #endregion
 
    #region Implementation -------------------------------------------
+   /// <summary>Returns the list of bend lines after translating them</summary>
+   // Each bendLine is moved towards the base by a sum of half of its bend deduction and 
+   // the total of the bend deduction of all the inner bendLines
+   List<BendLine> GetTranslatedBLines (List<BendLine> bLines, out double totalBD, out int horBLCount, out int verBLCount, bool isNegOff = false) {
+      var newBendLines = new List<BendLine> ();
+      var offFactor = isNegOff ? -1 : 1;
+      totalBD = 0.0;
+      horBLCount = verBLCount = 0;
+      foreach (var bl in bLines) {
+         var (bd, orient) = (bl.BLInfo.Deduction, bl.Orientation);
+         var offset = offFactor * (totalBD + 0.5 * bd);
+         (double dx, double dy) = (0, 0);
+         if (orient is Horizontal) {
+            horBLCount += 1;
+            (dx, dy) = (0.0, offset);
+         } else if (orient is Vertical) {
+            verBLCount += 1;
+            (dx, dy) = (offset, 0.0);
+         }
+         totalBD += bd;
+         newBendLines.Add ((BendLine)bl.Translated (dx, dy));
+      }
+      return newBendLines;
+   }
+
+   /// <summary>Returns the edges parallel to the bend lines</summary>
+   List<PLine> GetProfilePLines (Part part, EBLLocation loc, bool HasVerBLine, bool HasHorBLine) {
+      var b = part.Bound;
+      List<PLine> alignedPLines = [];
+      if (HasVerBLine) { // Handles vertical bend lines and returns nearest vertical pLine 
+         alignedPLines.Add (part.PLines.Where (c => loc is EBLLocation.Top ? c.StartPoint.X == b.MaxX && c.EndPoint.X == b.MaxX
+                                                                         : c.StartPoint.X == b.MinX && c.EndPoint.X == b.MinX).First ());
+      }
+
+      if (HasHorBLine) { // Handles horizontal bend lines and returns nearest horizontal pLine 
+         alignedPLines.Insert (0, part.PLines.Where (c => loc is EBLLocation.Top ? c.StartPoint.Y == b.MaxY && c.EndPoint.Y == b.MaxY
+                                                                               : c.StartPoint.Y == b.MinY && c.EndPoint.Y == b.MinY).First ());
+      }
+      return alignedPLines;
+   }
    #endregion
 
    #region Private Data ---------------------------------------------
-   readonly EBDAlgorithm mAlgorithm;
+   readonly EBDAlgorithm mAlgorithm; // Algorithm by which bend deduction is applied
    #endregion
 }
+#endregion
+
+// TO be moved to Model - BEnum.cs
+#region Enums -------------------------------------------------------------------------------------
+public enum EBLLocation { Top, Bottom }
 #endregion
