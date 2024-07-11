9863f91f69248779e9e9e1cfcc08349595ce7538
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2024-07-11 15:13:22 +0530

    Minor changes in CC

--- Src/BendAssist.App/BendAssists/CornerClose.cs
+++ Src/BendAssist.App/BendAssists/CornerClose.cs
@@ -8,7 +8,7 @@ namespace BendAssist.App.BendAssists;
 public sealed class CornerClose : BendAssist {
    #region Constructors ---------------------------------------------
    /// <summary>Gets the part and instantiates the members to accomplish corner closing</summary>
-   public CornerClose (Part part) => (mPart, mIndex, mCcVertices, mStepLines, mEdgeLines, mNewLines, mStepStartPts, mStepEndPts) = (part, -1, [], [], [], [], [], []);
+   public CornerClose (Part part) => (mPart, mIndex, mCcVertices, mStepLines, mNewLines, mStepStartPts, mStepEndPts) = (part, -1, [], [], [], [], []);
    #endregion
 
    #region Properties -----------------------------------------------
@@ -19,7 +19,7 @@ public sealed class CornerClose : BendAssist {
    #region Methods --------------------------------------------------
    /// <summary>Checks if a part is eligible for corner closing and gives the corner closed part</summary>
    public override void Execute () {
-      var (vertices, pLines, bendLines, centroid) = (mPart!.Vertices, mPart.PLines, mPart.BendLines.Cast<Line> ().ToList (), mPart.Centroid);
+      var (vertices, pLines, bendLines, centroid) = (mPart!.Vertices, mPart.PLines.Cast<Line> ().ToList (), mPart.BendLines.Cast<Line> ().ToList (), mPart.Centroid);
       var commonVertices = vertices.GroupBy (p => p).Where (p => p.Count () > 2).Select (g => g.Key).ToList (); // To find the intersecting points of bendlines with stepcut 
       if (commonVertices.Count < 1) { mAssistError = mErrorMsg; return; }
       foreach (var vertex in commonVertices) {
@@ -27,19 +27,46 @@ public sealed class CornerClose : BendAssist {
          if (connectedBLines.All (b => b.Orientation != Inclined)) mCcVertices.Add (vertex); // Checks if the bend lines connected to the common vertices are not inclined
       }
       if (mCcVertices.Count < 1) { mAssistError = mErrorMsg; return; }
+      foreach (var ccVertex in mCcVertices) {
+         ccVertex.IsCommonVertex (pLines, out var connectedPLines);
+         if (connectedPLines.Count == 2) {
+            var (line1, line2) = (connectedPLines[0], connectedPLines[1]);
+            if (line1.Index != line2.Index - 1) {
+               mStepLines.Add ((PLine)line2); mStepLines.Add ((PLine)line1);
+            } else connectedPLines.ForEach (x => mStepLines.Add ((PLine)x));
+         }
+      }
+      mStepLines.ForEach (line => {
+         mStepStartPts.Add (line.StartPoint);
+         mStepEndPts.Add (line.EndPoint);
+      });
       var (angle, kFactor, thickness, radius) = (90, 0.38, 2, 2);
       var halfBA = BendUtils.GetBendAllowance (angle, kFactor, thickness, radius) / 2;
       var lineShift = halfBA - radius;
       var halfBD = BendUtils.GetBendDeduction (angle, kFactor, thickness, radius) / 2;
-      foreach (var pLine in pLines)
-         if (mCcVertices.Any (pLine.HasVertex)) {
-            mStepLines.Add (pLine);
-            mStepStartPts.Add (pLine.StartPoint);
-            mStepEndPts.Add (pLine.EndPoint);
-         } else mEdgeLines.Add (pLine);
       foreach (var pLine in pLines) {
          var (startPt, endPt) = (pLine.StartPoint, pLine.EndPoint);
-         if (mEdgeLines.Contains (pLine)) {
+         if (mStepLines.Contains (pLine)) {
+            if (mStepLines.IndexOf ((PLine)pLine) % 2 == 0) {
+               PLine translatedLine = pLine.Orientation switch {
+                  Horizontal => startPt.Y < centroid.Y ? (PLine)pLine.Translated (0, lineShift) : (PLine)pLine.Translated (0, -lineShift),
+                  Vertical => startPt.X < centroid.X ? (PLine)pLine.Translated (lineShift, 0) : (PLine)pLine.Translated (-lineShift, 0),
+                  _ => null!
+               };
+               mNewLines.Add (new (translatedLine.StartPoint, translatedLine.EndPoint, ++mIndex));
+            } else {
+               (PLine extrudedLine, PLine trimmedLine) = pLine.Orientation switch {
+                  Horizontal => startPt.X < centroid.X ? ((PLine)pLine.Trimmed (startDx: -halfBA).Translated (0, halfBD), (PLine)pLine.Trimmed (startDx: lineShift, endDx: pLine.Length - halfBA))
+                                                                     : ((PLine)pLine.Trimmed (startDx: halfBA).Translated (0, -halfBD), (PLine)pLine.Trimmed (startDx: -lineShift, endDx: -(pLine.Length - halfBA))),
+                  Vertical => startPt.Y < centroid.Y ? ((PLine)pLine.Trimmed (startDy: -halfBA).Translated (-halfBD, 0), (PLine)pLine.Trimmed (startDy: lineShift, endDy: pLine.Length - halfBA))
+                                                                   : ((PLine)pLine.Trimmed (startDy: halfBA).Translated (halfBD, 0), (PLine)pLine.Trimmed (startDy: -lineShift, endDy: -(pLine.Length - halfBA))),
+                  _ => (null!, null!)
+               };
+               mNewLines.Add (new (trimmedLine.StartPoint, trimmedLine.EndPoint, ++mIndex));
+               mNewLines.Add (new (trimmedLine.EndPoint, extrudedLine.StartPoint, ++mIndex));
+               mNewLines.Add (new (extrudedLine.StartPoint, extrudedLine.EndPoint, ++mIndex));
+            }
+         } else {
             PLine trimmedEdge;
             if (mStepStartPts.Contains (endPt) && mStepEndPts.Contains (startPt)) {
                trimmedEdge = pLine.Orientation switch {
@@ -63,26 +90,6 @@ public sealed class CornerClose : BendAssist {
                };
                mNewLines.Add (new (trimmedEdge.StartPoint, trimmedEdge.EndPoint, ++mIndex));
             } else mNewLines.Add (new (pLine.StartPoint, pLine.EndPoint, ++mIndex));
-         } else {
-            if (mStepLines.IndexOf (pLine) % 2 == 0) {
-               PLine translatedLine = pLine.Orientation switch {
-                  Horizontal => startPt.Y < centroid.Y ? (PLine)pLine.Translated (0, lineShift) : (PLine)pLine.Translated (0, -lineShift),
-                  Vertical => startPt.X < centroid.X ? (PLine)pLine.Translated (lineShift, 0) : (PLine)pLine.Translated (-lineShift, 0),
-                  _ => null!
-               };
-               mNewLines.Add (new (translatedLine.StartPoint, translatedLine.EndPoint, ++mIndex));
-            } else {
-               (PLine extrudedLine, PLine trimmedLine) = pLine.Orientation switch {
-                  Horizontal => startPt.X < centroid.X ? ((PLine)pLine.Trimmed (startDx: -halfBA).Translated (0, halfBD), (PLine)pLine.Trimmed (startDx: lineShift, endDx: pLine.Length - halfBA))
-                                                                     : ((PLine)pLine.Trimmed (startDx: halfBA).Translated (0, -halfBD), (PLine)pLine.Trimmed (startDx: -lineShift, endDx: -(pLine.Length - halfBA))),
-                  Vertical => startPt.Y < centroid.Y ? ((PLine)pLine.Trimmed (startDy: -halfBA).Translated (-halfBD, 0), (PLine)pLine.Trimmed (startDy: lineShift, endDy: pLine.Length - halfBA))
-                                                                   : ((PLine)pLine.Trimmed (startDy: halfBA).Translated (halfBD, 0), (PLine)pLine.Trimmed (startDy: -lineShift, endDy: -(pLine.Length - halfBA))),
-                  _ => (null!, null!)
-               };
-               mNewLines.Add (new (trimmedLine.StartPoint, trimmedLine.EndPoint, ++mIndex));
-               mNewLines.Add (new (trimmedLine.EndPoint, extrudedLine.StartPoint, ++mIndex));
-               mNewLines.Add (new (extrudedLine.StartPoint, extrudedLine.EndPoint, ++mIndex));
-            }
          }
       }
       mProcessedPart = new (mNewLines, mPart.BendLines, 2, EBendAssist.CornerClose);
@@ -90,7 +97,7 @@ public sealed class CornerClose : BendAssist {
    #endregion
 
    #region Private Data ---------------------------------------------
-   List<PLine> mStepLines, mEdgeLines, mNewLines;
+   List<PLine> mStepLines, mNewLines;
    List<Point2> mCcVertices, mStepStartPts, mStepEndPts;
    int mIndex;
    string mErrorMsg = "Cannot apply corner closing";
